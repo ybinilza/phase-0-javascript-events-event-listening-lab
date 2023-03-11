a09ed2273afcba6c8c5f7a2676875e77fc57c66d
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-10 20:24:36 -0500

    Lab Completed

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-function addingEventListener() {
+/*const input = document.getElementById('button');
+input.addEventListener('click', addingEventListener);
+
+function addingEventListener()
+ {
+    
+
+ function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}*/
+
+
+function addingEventListener()
+ {
+
+    const input = document.getElementById('button');
+
+     function clickAlert()
+      {
+         alert('I was clicked!');
+      }
+
+      input.addEventListener('click', clickAlert);
+ }
+
+ addingEventListener();
